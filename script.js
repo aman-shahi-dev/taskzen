0f4dc4b5aea4b7adede252e8121c24395ab24e0a
Aman Shahi <amanshahidev@gmail.com>
2025-07-03 20:24:09 +0530

    created taskzen landing page and implemented the clock but have to implement timer, stopwatch and temperature

---
+++ script.js
@@ -0,0 +1,37 @@
+
+// Date and Time ka implementation
+
+const timeElement = document.querySelector('.currentTime')
+
+const dateElement = document.querySelector('.currentDate')
+
+function updateClock() {
+    const now = new Date();
+
+    const rawHours = now.getHours();
+    const hours = String(rawHours % 12 || 12).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const AmPm = rawHours >= 12 ? 'PM' : 'AM';
+
+    timeElement.innerHTML = `<h1>${hours}:${minutes}:${seconds} ${AmPm}</h1>`;
+
+    const date = now.getDate();
+    const month = now.getMonth();
+    const months = ['January', 'February', 'March', 'April', 'May', 'June',
+                    'July', 'August', 'September', 'October', 'November', 'December'];
+    const monthInWords = months[month];
+    const year = now.getFullYear();
+
+    dateElement.innerHTML = `<h1>${date} ${monthInWords} ${year}</h1>`;
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
+
+
+// Timer ka implementation
+
+// Stopwatch ka implementation
+
